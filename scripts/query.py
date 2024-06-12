8ff238ed8d3b31e295ab54df2e7b2f6a4b9cd563
Aswin kumar <kumarsaswin10@gmail.com>
2024-06-12 23:23:59 +1000

    pipeline for database and data

--- scripts/query.py
+++ scripts/query.py
@@ -1,14 +1,37 @@
 import sqlite3
 
-def queryDB(query):
+database = "../Vaccinations.db"
+
+def queryDB(query,param=None):
     try:
-        with sqlite3.connect("../Vaccinations.db") as conn:
+        with sqlite3.connect(database) as conn:
             cur = conn.cursor()
             if query != None:
-                cur.execute(query) 
+                if param == None:
+                    cur.execute(query) 
+                else:
+                    cur.execute(query,param)
                 row = cur.fetchall()
                 return row
             else:
                 return None
+    except sqlite3.Error as e:
+        print(e)
+
+def queryFile(filePath):
+    try:
+        conn = sqlite3.connect(database)
+        cur = conn.cursor()
+
+        # Read SQL commands from the file
+        with open(filePath, 'r') as file:
+            sql_commands = file.read()
+
+        # Execute SQL commands
+        cur.executescript(sql_commands)
+
+        # Commit changes and close the connection
+        conn.commit()
+        cur.close()
     except sqlite3.Error as e:
         print(e)
